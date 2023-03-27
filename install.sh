7dff141f587e8ef9e73c579f883818d4d39dc960
nashnoor <110758148+nashnoor@users.noreply.github.com>
2023-03-28 02:44:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "Cloning directory"
+git clone https://github.com/nashnoor/google-dorks-py.git
+cd google-dorks-py
 echo "Creating directory"
 mkdir downloads
 chmod 7777 downloads
-git clone https://github.com/nashnoor/google-dorks-py.git
-cd google-dorks-py
-echo "The current working directory is $PWD/downloads. Please enter the following directory in dorks.py *download_dir*
+echo "The current working directory is $PWD/downloads. Please enter the following directory in dorks.py  download_dir "
