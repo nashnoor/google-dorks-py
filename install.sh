455300da23a9b21ab1f78e836915d40216ffc3c0
nashnoor <110758148+nashnoor@users.noreply.github.com>
2023-03-28 02:53:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/nashnoor/google-dorks-py.git
 cd google-dorks-py
 echo "Creating directory"
 mkdir downloads
-chmod 7777 downloads
+chmod 7777 *
 echo "Running pip install"
 sudo pip install -r requirements.txt
 echo "The current working directory is $PWD/downloads. Please enter the following directory in dorks.py  download_dir "
